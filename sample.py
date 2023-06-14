c4e508624ee311527e8e7e58ea918b6fbfabcb78
krina1902 <krinaantala33@gmail.com>
2023-06-14 19:51:51 +0530

    py

---
+++ sample.py
@@ -0,0 +1,19 @@
+print ("hello")
+print ("im krina")
+print ("hello" , end = ", ")
+print ("im krina")
+a=10
+print("A : ",a)
+print(type(a))
+b="hello"
+print("A : ",b)
+print(type(b))
+c=10.567
+print("A : ",c)
+print(type(c))
+d=True
+print("A : ",d)
+print(type(d))
+e=False
+print("A : ",e)
+print(type(e))

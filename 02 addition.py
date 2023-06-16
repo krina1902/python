0fe6bb81cfd008446de43e1b1b0219aa7b6b10a4
krina1902 <krinaantala33@gmail.com>
2023-06-16 20:43:15 +0530

    py

--- 02 addition.py
+++ 02 addition.py
@@ -8,7 +8,12 @@ e=(input("enter the value of b:"))
 f=d+e
 print("concat addtion:",f)
 '''
-a=float(input("enter the value of a:"))
-b=float(input("enter the value of b:"))
-c=a+b
-print("addtion:",c)
+# a=float(input("enter the value of a:"))
+# b=float(input("enter the value of b:"))
+# c=a+b
+# print("addtion:",c)
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+c=a%b
+print("modulo:")
+
--- 05 forloop.py
+++ 05 forloop.py
@@ -1,2 +1,13 @@
-for i in range(10):
+'''for i in range(10):
+    print(i)
+    '''
+'''
+for i in range(1,10):
+    print(i)
+   '''
+'''
+for i in range(10,1,-1):
+    print(i)
+'''
+for i in range(1,100,10):
     print(i)

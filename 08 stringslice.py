f95352de0fc3004b19b1d87dbdcca782d5bf9df6
krina1902 <krinaantala33@gmail.com>
2023-06-17 18:33:21 +0530

    py

---
+++ 08 stringslice.py
@@ -0,0 +1,15 @@
+s = "Python Programming"
+
+print(s[::2])
+print(s[4:12:1])
+print(s[-14:-4:3])
+print(s[::-2])
+print(s[-17:-1:1])
+print(s[4:16:3])
+print(s[2:16:90])
+print(s[4:7:2])
+print(s[1:16])
+print(s[-17:-5])
+print(s[-13:-6:2])
+print(s[-1:-18:-3])
+print(s[5:16:2])

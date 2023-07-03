ada4b62d4ee270bc36c78e11b333f5641ef72d2f
krina1902 <krinaantala33@gmail.com>
2023-07-03 19:14:17 +0530

    py

--- 34tkinter.py
+++ 34tkinter.py
@@ -1,1 +1,14 @@
 from tkinter import *
+
+root=Tk()
+root.geometry("500x500")
+root.title("Desktop Application")
+
+l_id=Label(root,text="ID",font=("Forte",20))
+l_id.place(x=50,y=50)
+l_name=Label(root,text="Name",font=("Forte",20))
+l_name.place(x=50,y=100)
+l_dept=Label(root,text="Dpartment",font=("Forte",20))
+l_dept.place(x=50,y=150)
+l_salary=Label(root,text="Salary",font=("Forte",20))
+l_salary.place(x=50,y=200)
